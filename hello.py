30b5910184afa3685676daf74dbde3ad8305dcdb
muhilvarnan <muhilvarnan.v@gmail.com>
2019-03-13 15:58:35 +0530

    add pylint step in job

--- hello.py
+++ hello.py
@@ -4,10 +4,10 @@ Hello world flask app
 from flask import Flask, jsonify
 from modules import service_now
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
-@app.route('/')
+@APP.route('/')
 def hello_word():
     obj = service_now.Adapater("https://www.booknomads.com")
     return jsonify(obj.get_books())
